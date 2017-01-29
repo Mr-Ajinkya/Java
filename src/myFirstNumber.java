2c6cb7736fafb7e057d738a6729f2565959ae8be
Mr-Ajinkya <Mr-Ajinkya>
2017-01-29 16:00:49 +0530

    Second program myFirstNumber

---
+++ src/myFirstNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lenovo on 1/29/2017.
+ */
+public class myFirstNumber {
+    public static void main(String[] args){
+        int Number = 5;
+        System.out.println(Number);
+    }
+}
