33e455a5cce6857da9033915cba8ce7e94607d74
Mr-Ajinkya <Mr-Ajinkya>
2017-01-29 16:12:03 +0530

    Addition Program

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lenovo on 1/29/2017.
+ */
+public class Addition {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 12;
+        int mySecondNumber = 6;
+        int myThirdNumber = 2;
+
+        int Total = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("Total is::" + Total);
+    }
+
+}
