eef3c8592a1274a6a9b085f4968d933049ef99e9
Mr-Ajinkya <Mr-Ajinkya>
2017-02-04 19:41:23 +0530

    Sum of two numbers

---
+++ src/Sum.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo on 2/4/2017.
+ */
+public class Sum {
+
+    public static void main(String[] args){
+
+        int x,y;
+        x = 10;
+        y = 25;
+
+        int z = x + y;
+
+        System.out.println(z);
+    }
+}
