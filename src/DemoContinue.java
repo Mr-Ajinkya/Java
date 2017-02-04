dbb4ac3358a9463cedd2108125f36129c6dce0c8
Mr-Ajinkya <Mr-Ajinkya>
2017-02-04 20:47:28 +0530

    Continue statement

---
+++ src/DemoContinue.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ajinkya on 2/4/2017.
+ */
+public class DemoContinue {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--){
+            if(i>5)continue;
+            System.out.println(i  +  "");
+        }
+    }
+}
