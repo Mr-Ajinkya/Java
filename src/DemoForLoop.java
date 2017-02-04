1b044996d2d5d18a79ac1829a38646ab0d7a1963
Mr-Ajinkya <Mr-Ajinkya>
2017-02-04 20:18:48 +0530

    print number 1 to 10 using for loop

---
+++ src/DemoForLoop.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lenovo on 2/4/2017.
+ */
+public class DemoForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
