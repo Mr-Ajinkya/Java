32292095eb85c432ba0ab69cd809ff44a26215d2
Mr-Ajinkya <Mr-Ajinkya>
2017-02-04 20:13:55 +0530

    Use of while loop to print number

---
+++ src/DemoWhile.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ajinkya on 2/4/2017.
+ */
+public class DemoWhile {
+
+    public static void main(String[] args) {
+
+        int x;
+
+        x = 1; //number begin
+        while(x <= 10)
+        {
+            System.out.println(x);//Print X
+            x++; // increment x
+        }
+    }
+}
