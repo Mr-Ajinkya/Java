a27fb33d222dcdf76d6d9eb23fe24669d5b692d7
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2017-02-12 01:10:18 +0530

    Print star in triangular format

---
+++ StarPrint/src/Star.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Ajinkya Virkud on 2/12/2017.
+ */
+public class Star {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
