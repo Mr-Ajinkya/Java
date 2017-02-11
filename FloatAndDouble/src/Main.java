d35485b0728c767af104acdfe1e71045af98f394
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2017-02-11 16:56:09 +0530

    Convert a pound to kilogram

---
+++ FloatAndDouble/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by lenovo on 2/11/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int myIntValue = 5;
+        float myFloatValue =  5.4f;
+        double myDoublevalue = 5d;
+        System.out.println("My int vlaue" + myIntValue);
+        System.out.println("My float vlaue" + myFloatValue);
+        System.out.println("My double vlaue" + myDoublevalue);
+    }
+}
---
+++ PoundToKilogram/src/PoundToKilogra.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ajinkya on 2/11/2017.
+ */
+public class PoundToKilogra {
+    public static void main(String[] args) {
+
+        //Converted a give pound number into a kilogram
+        // 1 pound = 0.45359237d
+        double numPound = 200;
+        double convertedKilogram = numPound *  0.45359237d;
+        System.out.println("Kilograms " + convertedKilogram);
+    }
+}
