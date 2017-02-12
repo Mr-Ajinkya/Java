7b635a4b00d22d133dccc281a8f064a12e480def
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2017-02-12 20:30:04 +0530

    Check the number is positive or negative using if...else statement

---
+++ IfTheElse/src/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Ajinkya Virkud on 2/12/2017.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        //Check the number is positive or negative
+
+        int  number = 5;
+
+        if(number == 0){
+            System.out.println("Number is zero");
+        }else if (number > 0){
+            System.out.println("Number is positive");
+        }
+        else {
+            System.out.println("Number is negative");
+        }
+
+    }
+}
---
+++ MethodInJava/src/Method.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ajinkya Virkud on 2/12/2017.
+ */
+public class Method {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+
+
+    }
+}
