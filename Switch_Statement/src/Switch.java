66f9720192663393f0442ce00aac8bbaa5ebf30c
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2017-03-09 12:42:10 +0530

    Switch Statement

---
+++ Switch_Statement/src/Switch.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Ajinkya Virkud on 3/9/2017.
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+
+
+        int switchValue = 3;
+
+        switch(switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+    }
+}
