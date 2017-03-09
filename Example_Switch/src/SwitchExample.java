dc572ab57668f4e5b9d03dcd31648ae537b11219
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2017-03-09 13:19:16 +0530

    Switch Statement Example find Character

---
+++ Example_Switch/src/SwitchExample.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Ajinkya Virkud on 3/9/2017.
+ */
+public class SwitchExample {
+
+    public static void main(String[] args) {
+
+        char charValue = 'G';
+
+        switch (charValue){
+
+            case 'A':
+                System.out.println("A was found");
+                break;
+
+            case 'B':
+                System.out.println("B was found");
+                break;
+
+            case 'C':case 'D':case 'E':
+                System.out.println("A,B,C,D or E found");
+                break;
+
+            default:
+                System.out.println("Was not found");
+                break;
+
+        }
+
+
+    }
+
+}
